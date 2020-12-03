f30cd0673b83d1afd5a0ff8c5b1429488583d47a
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2020-12-03 18:19:33 +0300

    Forbid empty positioning ATX

--- activation/atxdb.go
+++ activation/atxdb.go
@@ -354,16 +354,13 @@ func (db *DB) SyntacticallyValidateAtx(atx *types.ActivationTx) error {
 	// 	return fmt.Errorf("empty previous ATX")
 	// }
 
-	// if atx.PositioningATX == *types.EmptyATXID {
-	// 	return fmt.Errorf("empty positioning ATX")
-	// }
+	if atx.PositioningATX == *types.EmptyATXID {
+		db.log.Warning("validating ATX with empty pos ATX ID, golden ATX: %v", db.goldenATXID)
+		return fmt.Errorf("empty positioning ATX")
+	}
 
 	if atx.PrevATXID == *types.EmptyATXID {
 		db.log.Warning("validating ATX with empty prev ATX ID, golden ATX: %v", db.goldenATXID)
-	}
-
-	if atx.PositioningATX == *types.EmptyATXID {
-		db.log.Warning("validating ATX with empty pos ATX ID, golden ATX: %v", db.goldenATXID)
 	}
 
 	if atx.PrevATXID != db.goldenATXID {
